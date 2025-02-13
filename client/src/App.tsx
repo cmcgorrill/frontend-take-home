b0b81679f1f9f42135c2c9f2babeaaff7b719b93
Courtney McGorrill <courtney@Courtneys-MacBook-Pro.local>
2025-02-13 18:11:53 -0500

    Task 1: Setup the Users and Roles tab structure DONE

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
-import { Container } from '@radix-ui/themes';
+import { Box, Container, Tabs, Text } from '@radix-ui/themes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Container></Container>
-    </div>
+    <Box className="App" style={{ padding: '8px 40px' }}>
+      <Container>
+        <Tabs.Root defaultValue="users">
+          <Tabs.List>
+            <Tabs.Trigger value="users">Users</Tabs.Trigger>
+            <Tabs.Trigger value="roles">Roles</Tabs.Trigger>
+          </Tabs.List>
+
+          <Box pt="3">
+            <Tabs.Content value="users">
+              <Text size="2">Got users?</Text>
+            </Tabs.Content>
+
+            <Tabs.Content value="roles">
+              <Text size="2">Role-ing up soon.</Text>
+            </Tabs.Content>
+
+          </Box>
+        </Tabs.Root>
+      </Container>
+    </Box>
   );
 }
 
